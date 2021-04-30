0bd0b7d0adf8558903450fee552083b94c6cb47a
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-30 23:29:04 +0530

    Conditional Statements in templates

--- BookStore/app.py
+++ BookStore/app.py
@@ -1,13 +1,16 @@
-from flask import Flask, redirect, url_for,request, render_template
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/profile/<username>')
-def profile(username):
-    return render_template('profile.html', username = username)
 
-app.run(debug = True)
+@app.route("/profile/<username>")
+def profile(username):
+    return render_template('profile.html', username=username, isActive=False)
+
+
+app.run(debug=True)
