6dea14996e3c071e0c7eb1e3fc8a5a4c9ca8c029
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-30 09:53:59 +0530

    [ADD] Redirect/url_for

--- BookStore/app.py
+++ BookStore/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
+
 @app.route('/admin')
 def admin():
     return 'welcome admin'
 
-@app.route('/guest/')
+@app.route('/guest/<username>')
+def guest(username):
+    return 'welcome %s' % username
+
+@app.route('/profile/<guest>')
+def add(guest):
+    if guest == 'admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest', username = guest))
+
+app.run(debug = True)
