06ec57c6313a2678904b2f1bba218299750b250a
tanya1019 <tanya.vanneri19@gmail.com>
2021-04-30 17:04:54 +0530

    Passing dynamic data to templates

--- BookStore/app.py
+++ BookStore/app.py
@@ -2,43 +2,12 @@ from flask import Flask, redirect, url_for,request, render_template
 
 app = Flask(__name__)
 
-@app.route('/admin')
-def admin():
-    return 'welcome admin'
-
-@app.route('/guest/<username>')
-def guest(username):
-    return 'welcome %s' % username
-
-@app.route('/profile/<guest>')
-def add(guest):
-    if guest == 'admin':
-        return redirect(url_for('admin'))
-    else:
-        return redirect(url_for('guest', username = guest))
-
-@app.route('/abc')
-def abc():
-    return 'this is request page %s' % request.headers
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', username = username)
 
 app.run(debug = True)
