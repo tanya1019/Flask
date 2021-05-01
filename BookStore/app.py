6c72e2c1063add55784411915650bf70ae2472b9
tanya1019 <tanya.vanneri19@gmail.com>
2021-05-01 22:58:24 +0530

    Configured Database using sqlite

--- BookStore/app.py
+++ BookStore/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, url_for, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = 'sqlite:///{}'.format(
+    os.path.join(project_dir, 'mydatabase.db'))
 
 app = Flask(__name__)
+
+app.config('SQLAlchemy_DATABASE_URI') = database_file
+db = SQLAlchemy(app)
 
 
 @app.route('/')
